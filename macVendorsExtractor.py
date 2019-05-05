c6ab214b447d6e745277eab9f9e166f91b9c25e6
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2019-05-04 21:48:14 -0300

    feat: script para extração de endereços MAC e arquivo com endereços

---
+++ macVendorsExtractor.py
@@ -0,0 +1,10 @@
+def getMacVendors():
+    vendors = {}
+    arq = open('mac-vendor.txt', 'r')
+    lines = arq.readlines()
+
+    for macVendor in lines:
+        info = macVendor.split('\t')
+        vendors[info[0]] = info[1].replace('\n', '')
+
+    return vendors
